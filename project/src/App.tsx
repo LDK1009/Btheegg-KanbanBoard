3f73c5c43bc790c97db0f8ebdfdfbc492c5f3713
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-02-08 14:57:32 +0900

    ✨ FEAT : 모바일 DnD 적용

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,11 +1,14 @@
+import { TouchBackend } from "react-dnd-touch-backend";
 import Home from "./pages/Home";
 import GlobalStyle from "./styles/GlobalStyle";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 function App() {
+  const isMobile = "ontouchstart" in window;
+
   return (
     <>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
         <GlobalStyle />
         <Home />
       </DndProvider>
